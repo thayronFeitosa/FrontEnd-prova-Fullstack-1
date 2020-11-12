f2c78b1d7b181bd5a818c5bd6b7a5a2551206498
thayron <thayronf3@gmail.com>
2020-11-12 00:16:59 -0300

    Foi iniciado  o projeto e iniciado as migrations

---
+++ database/migrations/2020_11_12_000223_create_table_telephone.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableTelephone extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telephone', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nunber', 20);
+            $table->integer('user_id')->unsigned();	
+            $table->foreign('user_id')->references('id')->on('user');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telephone');
+    }
+}
